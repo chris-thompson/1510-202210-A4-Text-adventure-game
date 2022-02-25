e7e9d1518534165582d2ff76d23d40f1092775e6
Chris <chris_thompson@bcit.ca>
2022-02-24 20:13:23 -0800

    Hello world!

---
+++ game.py
@@ -0,0 +1,17 @@
+"""
+Do not remove this file. Ensure your main function is in this file.
+
+I will execute your game like this:
+
+python3 game.py
+
+Yes, you may add additional files to this folder.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
